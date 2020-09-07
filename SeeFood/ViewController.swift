b8df5745747c10b22e55268d578e85ab370768b9
Ameya Bhagat <techliteral@outlook.com>
2020-09-07 19:13:58 +0530

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Ameya Bhagat on 07/09/20.
+//  Copyright © 2020 Ameya Bhagat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
